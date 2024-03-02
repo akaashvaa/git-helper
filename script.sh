2c41288fe31d74397278883b15bb9e51d6836010
AkashKumar-0 <ahmvaad@gmail.com>
2024-03-02 10:51:05 +0530

    update script

--- script.sh
+++ script.sh
@@ -2,23 +2,15 @@
 
 if [ $# -eq 0 ]; then
 	echo "Error: Please provide a suitable argument to proceed."
-	echo "Use 'ghp --help' to know more."
+	echo "Use 'npm run ghp -- help' to know more."
 	exit 1
 fi
 
 case "$1" in
-"ghp")
-	case "$2" in
-	"--help")
-		echo " ------------ Instructions ------------ "
-		echo "1. To add a new repository: ghp your_github_url_for_new_project branch-optional"
-		echo '2. To push to an existing repository: ghp commit_msg or ghp "commit msg" or gsnap commit msg \n '
-		;;
-	*)
-		echo "Error: Invalid argument. Use 'ghp --help' to know more."
-		exit 1
-		;;
-	esac
+"help")
+	echo " ------------------------------------- Instructions ----------------------------------- "
+	echo "1. To add a new repository: npm run ghp -- your_github_url_for_new_project commit_msg(optional)"
+	echo '2. To push to an existing repository: npm run ghp -- commit_msg or "commit msg" or commit msg '
 	;;
 https*://*.git | git@*.git)
 	message="first commit"
@@ -37,7 +29,7 @@ https*://*.git | git@*.git)
 	;;
 *)
 	echo "Error: didn't match with the followed pattern. "
-	echo "Use 'ghp --help' to know more."
+	echo "Use 'npm run ghp -- help' to know more."
 	exit 1
 	;;
 esac
